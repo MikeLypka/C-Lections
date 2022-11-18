4643bb530b48b7db1bef71ed01a6a3d42e32f8f6
Michael Lypka <lypka.michael@gmail.com>
2022-11-17 21:51:41 -0500

    Change Task2 to Task4 and updated

---
+++ HomeWork/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter the first number: ");
+string numA = Console.ReadLine();
+int a = Convert.ToInt32(numA);
+
+Console.Write("Enter the second number: ");
+string numB = Console.ReadLine();
+int b = Convert.ToInt32(numB);
+
+Console.Write("Enter the third number: ");
+string numC = Console.ReadLine();
+int c = Convert.ToInt32(numC);
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("The max number is: ");
+Console.WriteLine(max);
