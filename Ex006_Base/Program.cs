7f11d63264479926c5ed4e52f5d3c64af3578171
Michael Lypka <lypka.michael@gmail.com>
2022-11-17 20:31:18 -0500

    Updated Ex006

--- Ex006_Base/Program.cs
+++ Ex006_Base/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 2;
+int c = 9;
+int d = 4;
+int e = 5;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
