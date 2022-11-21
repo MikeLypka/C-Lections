51a2ac03bd83eab05ad15bed34d4e8ffe6f126c9
Michael Lypka <lypka.michael@gmail.com>
2022-11-21 00:01:05 -0500

    updated task 6 and 8

--- HomeWork/Task6/Program.cs
+++ HomeWork/Task6/Program.cs
@@ -1,6 +1,5 @@
 ﻿Console.Write("Enter a number: ");
-// string n = Console.ReadLine();
-// int num = Convert.ToInt32(n);
+
 int n = int.Parse(Console.ReadLine()!);
 if (n % 2 == 0)
 {
--- HomeWork/Task8/Program.cs
+++ HomeWork/Task8/Program.cs
@@ -1,15 +1,21 @@
 ﻿Console.Write("Enter a number: ");
 int n = int.Parse(Console.ReadLine()!);
 
-int n2 = 1;
+// int n2 = 1;
 
-while(n2 <= n)
+// while(n2 <= n)
+// {
+//     if(n2 % 2 == 0)
+//     Console.Write($"{n2} ");
+//     n2 += 1;
+// }
+
+for (int i = 1; i <= n; i++)
 {
-    if(n2 % 2 == 0)
-    Console.Write($"{n2} ");
-    n2 += 1;
-    
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
 }
-
 
 
