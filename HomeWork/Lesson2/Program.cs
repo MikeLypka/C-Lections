baf2a784df496c7e28ab54deda6c0650c7776909
Michael Lypka <lypka.michael@gmail.com>
2022-11-21 23:52:50 -0500

    added the Homework for lesson 2

--- HomeWork/Lesson2/Program.cs
+++ HomeWork/Lesson2/Program.cs
@@ -1,2 +1,54 @@
-﻿// Task 10
+﻿/*Task 10
 
+// Console.Write("Enter three-digit number: ");
+// string Number = Console.ReadLine()!;
+
+// if (Number.Length != 3)
+// {
+//     Console.WriteLine("You entered not a three-digit number!");
+// }
+// else
+// {
+//     Console.WriteLine(Number[1]);
+// }
+*/
+
+/*
+Task 13
+
+Console.Write("Enter three-digit number: ");
+string Number = Console.ReadLine()!;
+
+if (Number.Length < 3)
+{
+    Console.WriteLine($"There not third digit in {Number}");
+    
+}
+else
+{
+    Console.WriteLine(Number[2]);
+}
+*/
+
+/*
+Task 13
+
+Console.Write("Enter a number: ");
+int Number = int.Parse(Console.ReadLine()!);
+
+if (Number > 0 && Number <= 7)
+{
+    if (Number == 6 || Number == 7)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
+else
+{
+    Console.WriteLine("Enter correct number!");
+}
+*/
