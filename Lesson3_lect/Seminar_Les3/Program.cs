8e2a5b6940248195008a47cfffe662523f2a44ca
Michael Lypka <lypka.michael@gmail.com>
2022-11-25 12:05:06 -0500

    added seminar3 to Lesson3

---
+++ Lesson3_lect/Seminar_Les3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter a qourter: ");
+int num = int.Parse(Console.ReadLine()!);
+
+switch(num)
+{
+    case 1:
+    Console.WriteLine("This is the first qourter");
+    break;
+
+    case 2:
+    Console.WriteLine("This is the second quarter");
+    break;
+
+    case 3:
+    Console.WriteLine("This is the third quarter");
+    break;
+
+    case 4:
+    Console.WriteLine("This is the fourth quarter");
+    break;
+
+    default: Console.WriteLine("There a no quarter you enter");
+    break;
+}
