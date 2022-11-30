8cfcf2959548190da8fb24f467528a5c682c0ffe
Michael Lypka <lypka.michael@gmail.com>
2022-11-29 20:41:44 -0500

    Added HomeWork for lesson 4 and test directory

---
+++ Seminar_Les4/Program.cs
@@ -0,0 +1,104 @@
+﻿// // Console.Write("Enter a number: ");
+// // int userNum = int.Parse(Console.ReadLine()!); 
+
+// // int SumCount(int num){ // Начало метода
+
+// //     int Result = 0;
+// //     for (int i = 1; i <= num; i++)
+// //     {
+// //         Result += i;
+// //         // Console.Write($"{i} ");
+// //         // Console.WriteLine($"{Result} ");
+// //     }
+// //     return Result;
+// // }
+
+// // Console.WriteLine(SumCount(userNum));
+
+
+// //Task 1 принимает на вход число нужно вывести сколько цифр в числе
+
+// Console.Write("Enter a number: ");
+// int num = int.Parse(Console.ReadLine()!);
+
+// int DigitCount(int num){
+//     // int DigitCount = Convert.ToInt32(Math.Log10(userNum) + 1);
+//     int counter = 0;
+
+//     while (num > 0)
+//     {
+//         counter++;
+//         num = num / 10;
+//     }
+//     return counter;
+// }
+
+// Console.WriteLine($"Number is consist of {DigitCount(num)} digits");
+
+//Task 2
+// int[] array = new int[8];
+// Mass(array);
+// Console.WriteLine($"Massive: {String.Join(" ", array)}");
+
+// int[] Mass(int[] array){
+//     for (int i = 0; i < array.Length; i++){
+//         array[i] = new Random().Next(0, 2);
+//     }
+//     return array;
+// }
+
+// int[] array = Getnumber(8);
+// Console.WriteLine($"[{String.Join("-", array)}]");
+
+// int[] Getnumber(int size)
+// {
+//     int[] result = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         result[i] = new Random().Next(2);
+//     }
+//     return result;
+// }
+
+// Task 3 Найти произведение всех элементов массива целых чисел, меньших заданного числа.
+// Размерность массива - 10. Заполнение массива осуществить случайными числами от 50 до 100.
+
+Console.Write("Enter a number: ");
+int num = int.Parse(Console.ReadLine()!);
+// int[] array = MultNumbers(num);
+array(mass);
+Console.WriteLine($"Massive: {String.Join(" ", array)}");
+// Console.WriteLine($"Произведение эл-тов массива {String.Join(", ", array)} {MultArray}");
+
+int[] array(int num) // создал метод
+{   
+    int[] mass = new int[10]; // Создал пустой массив размером size
+    // int total = 0; //сумма перемноженных элементов массива
+
+    for (int i = 0; i < 10; i++)
+    {   mass[i] = new Random().Next(50, 101); // наполнил массив цефрами от 50 до 100
+        // if (mass[i] < num)
+        // {
+        //     total += mass[i] * mass[i + 1]; // результат произведения всех элементов массива, меньше num
+        // }
+    }
+    return mass;
+}
+
+// int MultArray(int[] array, int num)
+// {
+//     int total = 1; //сумма перемноженных элементов массива
+//     for (int i = 0; i < 10; i++)
+//     {   
+//         if (array[i] < num)
+//         {
+//             total += mass[i] * mass[i + 1];
+//         }
+//         mass[i] = new Random().Next(50, 101); // наполнил массив цефрами от 50 до 100
+//         // if (mass[i] < num)
+//         // {
+//         //     total += mass[i] * mass[i + 1]; // результат произведения всех элементов массива, меньше num
+//         // }
+//     }
+//     return total;
+// }
---
+++ Test/Program.cs
@@ -0,0 +1,39 @@
+﻿
+// 1. Найти позицию минимального элемента в неотсортированной части массива.
+// 2. Произвести обмен этого значения со значением первой неотсортированной позиции.
+// 3. Повторять пока есть неотсортированные элементы.
+
+int[] MyArray = {6, 8, 3, 2, 1, 4, 1, 7, 7};
+
+void PrintArray(int[] array)
+{
+    int minDigit = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}   
+
+PrintArray(MyArray);
+
+void ArraySort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minDigit = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minDigit]) minDigit = j;      
+        }
+        
+        int temp = array[i];
+        array[i] = array[minDigit];
+        array[minDigit] = temp;
+    }
+}
+
+ArraySort(MyArray);
+
+PrintArray(MyArray);
