a180e7fab69cbd38fdcba292ee729030c4f9cb1a
Michael Lypka <lypka.michael@gmail.com>
2023-01-26 23:38:03 -0500

    some changes

---
+++ HomeWork/Lesson8/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 54: 
+/*
+Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+Console.WriteLine("Type rows amount: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Type columns amount: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+
+PrintArray(array);
+Console.WriteLine();
+
+int[,] GetArray(int m, int n, int minValue, int maxValue){
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random(minValue, maxValue);
+        }
+    }
+}
+
+void PrintArray(int[,] array){
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortArray(int[,] array){
+    
+}
---
+++ Seminar_Less8/Program.cs
@@ -0,0 +1,43 @@
+﻿
+Console.WriteLine("Type rows amount: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Type columns amount: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] arrayFirst = GetArray(rows, columns, 0, 10);
+
+PrintArray(array);
+Console.WriteLine();
+
+int[,] GetArray(int m, int n, int minValue, int maxValue){
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+
+void SortArray(int[,] array){
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int temp = array[0, j];
+        array[0, j] = array[array.GetLength(0), j];
+        array[array.GetLength(0), j] = temp;
+    }
+}
+
+void PrintArray(int[,] array){
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
